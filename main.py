6103c40583bda6296cab8e8b70e2cf4ea456eb6b
Hunter Achieng <hunter.achieng@twiga.com>
2022-05-21 18:37:01 +0300

    implement sorting again

--- main.py
+++ main.py
@@ -1,40 +1,72 @@
 import csv
+from abc import ABC, abstractmethod
+
+class CSVSort(ABC):
+
+    @abstractmethod
+    def sortData(self):
+        pass
+
+
+    # @abstractmethod
+    # def search(self):
+    #     pass
+
+
+
+
+class MergeSort(CSVSort):
+   
+    
+    def  __init__(self, data):
+        self.data = data
+    
+    def sortData(self):
+        if len(self.data) >1:
+            middle = len(self.data)//2
+            left =  self.data[1:middle]
+            right = self.data[middle:]
+
+            leftSort = MergeSort(left)
+            leftSort.sortData()
+
+            rigthSort = MergeSort(right)
+            rigthSort.sortData()
+
+           
+            
+
+            i = j=k=0
+            while i < len(left) and j < len(right):
+                if left[i][1] < right[j][1]:
+                    self.data[k] = left[i]
+                    i +=1
+                
+                else:
+                    self.data[k]=right[j]
+                    j+=1
+                k+=1
+            
+            while i < len(left):
+                self.data[k] = left[i]
+                i+=1
+                k+=1
+            
+            while j < len(right):
+                self.data[k] = right[j]
+                j+=1
+                k+=1
+        
+        print(self.data)
+    
+
+
 data = []
 with open('real-estate-sample.csv', 'r') as file:
-    reader = csv.reader(file)
-    data.extend(list(reader))
-    # print(data)
-    def merge(left, right):
-        if len(left) <=0 or len(right) <=0:
-            return left or right
-        
-        sorted_data = []
-        i = j=0
-        while len(sorted_data) < len(left) + len(right):
-            if left[i][1] < right[j][1]:
-                sorted_data.append(left[i])
-                i+=1
-            else:
-                sorted_data.append(right[j])
-                j+=1
-            if i == len(left) or j == len(right):
-                sorted_data.extend(left[i:] or right[j])
-                break
-        return sorted_data
-
-
-    def estateSort(data):
-        if len(data) < 2:
-            return data 
-        middle = len(data)//2
-        print(middle)
-        left =  estateSort(data[1:middle])
-        right = estateSort(data[middle:])
-        # print(left)
-
-        return merge(left,right)
-    print(estateSort(data))
-
-
+            reader = csv.reader(file)
+            data.extend(list(reader))
+            # print(data)
+sorted = MergeSort(data)
+print(sorted.sortData())
 
    
