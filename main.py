002759eda26031dffd483a19e742aae45ccdfdeb
Hunter Achieng <hunter.achieng@twiga.com>
2022-05-23 15:43:30 +0300

    CSV Sorting

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
 from sorting import MergeSort
-data = []
+sorted_data = []
 with open('real-estate-sample.csv', 'r') as file:
             reader = csv.reader(file)
-            data.extend(list(reader))
-sorted = MergeSort(data)
+            sorted_data.extend(list(reader))
+sorted = MergeSort(sorted_data)
 print(sorted.sortData())
