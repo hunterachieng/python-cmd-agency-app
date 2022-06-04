29cc6019459c5e54c1582a3b60d261979c217e95
Hunter Achieng <hunter.achieng@twiga.com>
2022-06-04 10:48:21 +0300

    implement click function to searching

--- venv/cmd_project/main.py
+++ venv/cmd_project/main.py
@@ -7,9 +7,10 @@ def search_csv (file_name, target, finder):
     with open(file_name, "r") as file:
         reader = csv.DictReader(file)
         data = list(reader)
+      
         things = SearchCSV(data,target, finder)
-        things.search()
-        print(things)
-    pdb.set_trace()
-search_csv("real-estate-sample.csv", '10000890', 'id')
+        print(things.search())
+     
+   
+search_csv("real-estate-sample.csv", 'NULL', 'providerListingId')
 
